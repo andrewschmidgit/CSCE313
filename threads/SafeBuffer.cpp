5297a98812e0bd91e24b9a2965a180819508c44e
andrewschmidgit <andrew.schmid123@gmail.com>
2018-10-20 11:16:21 -0500

    Creating files for threading

---
+++ threads/SafeBuffer.cpp
@@ -0,0 +1,45 @@
+#include "SafeBuffer.h"
+#include <string>
+#include <queue>
+using namespace std;
+
+SafeBuffer::SafeBuffer() {
+	
+}
+
+SafeBuffer::~SafeBuffer() {
+	
+}
+
+int SafeBuffer::size() {
+	/*
+	Is this function thread-safe???
+	Make necessary modifications to make it thread-safe
+	*/
+    pthread_mutex_lock(&lock);
+    int ret = q.size();
+    pthread_mutex_unlock(&lock);
+    return ret;
+}
+
+void SafeBuffer::push(string str) {
+	/*
+	Is this function thread-safe???
+	Make necessary modifications to make it thread-safe
+	*/
+    pthread_mutex_lock(&lock);
+	q.push (str);
+    pthread_mutex_unlock(&lock);
+}
+
+string SafeBuffer::pop() {
+	/*
+	Is this function thread-safe???
+	Make necessary modifications to make it thread-safe
+	*/
+    pthread_mutex_lock(&lock);
+	string s = q.front();
+	q.pop();
+    pthread_mutex_unlock(&lock);
+	return s;
+}
