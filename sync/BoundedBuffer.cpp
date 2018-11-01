dc6370ae5834be32a34079b5dcd26f8592d29d9a
Andrew Schmid <andrew.schmid123@gmail.com>
2018-10-31 22:16:46 -0500

    e

--- sync/BoundedBuffer.cpp
+++ sync/BoundedBuffer.cpp
@@ -29,26 +29,26 @@ int BoundedBuffer::size()
 
 void BoundedBuffer::push(string str)
 {
-    while(q.size() >= _capacity) {
+    pthread_mutex_lock(&_lock);
+    while(q.size() == _capacity) {
         cout << "Waiting to push: " << q.size() << _capacity << endl;
         pthread_cond_wait(&_max, &_lock);
     }
-    pthread_mutex_lock(&_lock);
     q.push(str);
-    pthread_mutex_unlock(&_lock);
     pthread_cond_signal(&_min);
+    pthread_mutex_unlock(&_lock);
 }
 
 string BoundedBuffer::pop()
 {
+    pthread_mutex_lock(&_lock);
     while(q.size() == 0) {
         cout << "Waiting to pop: " << q.size() << endl;
         pthread_cond_wait(&_min, &_lock);
     }
-    pthread_mutex_lock(&_lock);
     string s = q.front();
     q.pop();
-    pthread_mutex_unlock(&_lock);
     pthread_cond_signal(&_max);
+    pthread_mutex_unlock(&_lock);
     return s;
 }
