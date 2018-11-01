65f08e94173f60fd5d8069de0b6aa5971bc146dc
Andrew Schmid <andrew.schmid123@gmail.com>
2018-11-01 01:32:33 -0500

    try again

--- sync/test.sh
+++ sync/test.sh
@@ -14,59 +14,51 @@ make
 echo "Running tests..."
 echo "Worker threads,Buffer size,Time" >> output.txt
 
-echo "1,1" >> output.txt
-	./client -t -n 10000 -w 1 -b 1 >> output.txt
-
+echo "0,0," >> output.txt
+./client -t -n 10000 -w 0 -b 0 >> output.txt
 echo progress
-
-echo "10,25" >> output.txt
-	./client -t -n 10000 -w 10 -b 25 >> output.txt
-echo "20,50" >> output.txt
-	./client -t -n 10000 -w 20 -b 50 >> output.txt
-echo "30,75" >> output.txt
-	./client -t -n 10000 -w 30 -b 75 >> output.txt
-echo "40,100" >> output.txt
-	./client -t -n 10000 -w 40 -b 100 >> output.txt
-echo "50,125" >> output.txt
-	./client -t -n 10000 -w 50 -b 125 >> output.txt
-
+echo "10,25," >> output.txt
+./client -t -n 10000 -w 10 -b 25 >> output.txt
+echo "20,50," >> output.txt
+./client -t -n 10000 -w 20 -b 50 >> output.txt
+echo "30,75," >> output.txt
+./client -t -n 10000 -w 30 -b 75 >> output.txt
+echo "40,100," >> output.txt
+./client -t -n 10000 -w 40 -b 100 >> output.txt
+echo "50,125," >> output.txt
+./client -t -n 10000 -w 50 -b 125 >> output.txt
+echo "60,150," >> output.txt
+./client -t -n 10000 -w 60 -b 150 >> output.txt
+echo "70,175," >> output.txt
+./client -t -n 10000 -w 70 -b 175 >> output.txt
 echo progress
-
-echo "60,150" >> output.txt
-	./client -t -n 10000 -w 60 -b 150 >> output.txt
-echo "70,175" >> output.txt
-	./client -t -n 10000 -w 70 -b 175 >> output.txt
-echo "80,200" >> output.txt
-	./client -t -n 10000 -w 80 -b 200 >> output.txt
-echo "90,225" >> output.txt
-	./client -t -n 10000 -w 90 -b 225 >> output.txt
-echo "100,250" >> output.txt
-	./client -t -n 10000 -w 100 -b 250 >> output.txt
-echo "110,275" >> output.txt
-	./client -t -n 10000 -w 110 -b 275 >> output.txt
-echo "120,300" >> output.txt
-	./client -t -n 10000 -w 120 -b 300 >> output.txt
-
+echo "80,200," >> output.txt
+./client -t -n 10000 -w 80 -b 200 >> output.txt
+echo "90,225," >> output.txt
+./client -t -n 10000 -w 90 -b 225 >> output.txt
+echo "100,250," >> output.txt
+./client -t -n 10000 -w 100 -b 250 >> output.txt
+echo "110,275," >> output.txt
+./client -t -n 10000 -w 110 -b 275 >> output.txt
+echo "120,300," >> output.txt
+./client -t -n 10000 -w 120 -b 300 >> output.txt
 echo progress
-
-echo "130,325" >> output.txt
-	./client -t -n 10000 -w 130 -b 325 >> output.txt
-echo "140,350" >> output.txt
-	./client -t -n 10000 -w 140 -b 350 >> output.txt
-echo "150,375" >> output.txt
-	./client -t -n 10000 -w 150 -b 375 >> output.txt
-echo "160,400" >> output.txt
-	./client -t -n 10000 -w 160 -b 400 >> output.txt
-echo "170,425" >> output.txt
-	./client -t -n 10000 -w 170 -b 425 >> output.txt
-
+echo "130,325," >> output.txt
+./client -t -n 10000 -w 130 -b 325 >> output.txt
+echo "140,350," >> output.txt
+./client -t -n 10000 -w 140 -b 350 >> output.txt
+echo "150,375," >> output.txt
+./client -t -n 10000 -w 150 -b 375 >> output.txt
+echo "160,400," >> output.txt
+./client -t -n 10000 -w 160 -b 400 >> output.txt
+echo "170,425," >> output.txt
+./client -t -n 10000 -w 170 -b 425 >> output.txt
 echo progress
-
-echo "180,450" >> output.txt
-	./client -t -n 10000 -w 180 -b 450 >> output.txt
-echo "190,475" >> output.txt
-	./client -t -n 10000 -w 190 -b 475 >> output.txt
-echo "200,500" >> output.txt
-	./client -t -n 10000 -w 200 -b 500 >> output.txt
+echo "180,450," >> output.txt
+./client -t -n 10000 -w 180 -b 450 >> output.txt
+echo "190,475," >> output.txt
+./client -t -n 10000 -w 190 -b 475 >> output.txt
+echo "200,500," >> output.txt
+./client -t -n 10000 -w 200 -b 500 >> output.txt
 
 echo "Finished!"
